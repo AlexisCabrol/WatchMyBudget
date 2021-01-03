5aad063b455a703f5242301379f6b2b4dbc40fd5
Alexis Cabrol Olivaux <alexiscabrololivaux@macbook-pro-de-alexis.home>
2021-01-03 21:12:33 +0100

    Initial commit for API

---
+++ wmb-api/src/main/java/wmb/WatchMyBudget/WatchMyBudgetApplication.java
@@ -0,0 +1,13 @@
+package wmb.WatchMyBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchMyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchMyBudgetApplication.class, args);
+	}
+
+}
---
+++ wmb-api/src/test/java/wmb/WatchMyBudget/WatchMyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package wmb.WatchMyBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchMyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
